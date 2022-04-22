9adbc3d6b6b3c6d274b5d3ef01b23d6233bb8cfb
Steven <33363109+StevenDLL@users.noreply.github.com>
2022-04-22 13:27:40 -0400

    Add files via upload

--- Assembly Programming/Project 1/NumberConversionProject.asm
+++ Assembly Programming/Project 1/NumberConversionProject.asm
@@ -1,15 +1,19 @@
 	.data
 
 menu:		.asciiz	"1. Binary to Hexadecimal and Decimal \n2. Hexadecimal to Binary and Decimal \n3. Decimal to Binary and Hexadecimal \n4. Exit\n"
-error:		.asciiz	"\nPLEASE ENTER A VALUE BETWEEN 1-4\n"
+error:		.asciiz	"\nINVALID INPUT\n"
 binary:		.asciiz	"\nBinary Number: "
 hexadecimal:	.asciiz	"\nHexadecimal Number: "
-decimal:		.asciiz	"\nDecimal Number: "
+decimal:	.asciiz	"\nDecimal Number: "
 
-one:		.asciiz	"1"
-two:		.asciiz "2"
-three:		.asciiz	"3"
-four:		.asciiz	"4"
+one:		.word	1
+two:		.word 	2
+three:		.word	3
+four:		.word	4
+
+buffer:		.space	10
+binary_num:	.space 	10
+hexa
 
 	.text
 
@@ -24,27 +28,25 @@ display_menu:
 	syscall
 	
 	#Read Input
-	li $v0, 8
-	#Set MAX Characters to read
-	li $a1, 30
+	li $v0, 5
 	syscall
 	
 	#Move input
 	move $t0, $v0
 	
 	#Load Options Address
-	la $t1, one
-	la $t2, two
-	la $t3, three
-	la $t4, four
+	lw $t1, one
+	lw $t2, two
+	lw $t3, three
+	lw $t4, four
 	
 	beq $t0, $t1, from_binary
 	beq $t0, $t2, from_hexadecimal
 	beq $t0, $t3, from_decimal
 	beq $t0, $t4, exit
 	
-	la $a0, error
 	li $v0, 4
+	la $a0, error
 	syscall
 	
 	j display_menu
@@ -63,14 +65,32 @@ from_binary:
 	li $v0, 4
 	syscall
 	
-	#Take Input
+	#Read Input
 	li $v0, 8
+	#Set Address of input buffer
+	la $a0, buffer
 	#Set MAX Characters to read
 	li $a1, 30
 	syscall
+		
+	#Move value to a0
+	move $a0, $v0
 	
-	#Move value to t0(temporary register)
-	move $t0, $v0
+	#Make space in stack, save ra, then jump to verify
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	jal verify_binary
+	
+	#Make space in stack, save ra, then do conversions here
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	#Print new values here
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	jal display_results
 	
 	#Exit back to menu
 	j display_menu
@@ -82,8 +102,48 @@ from_binary:
 #If it meets all criteria then jump back to from_binary and continue
 #If it DOES'T meet all criteria then jump to invalid_input method, display message and then jump back to display_menu 
 verify_binary:
+	move $t0, $a0
+	
+	#Save original buffer string Address
+	la $s0, buffer
+	
+	#Set index
+	addi $s1, $zero, 0
+	
 
+verify_loop:
 
+	#Set t1 to address of buffer[i]
+	add $t1, $s0, $s1
+	
+	#Set t2 to the contents of buffer[i]
+	lbu $t2, 0($t1)
+	
+	#Increase index
+	addi $s1, $s1, 1
+	
+	#Check if content = [NULL]
+	beq $t2, $zero, exit
+	#Check if content = [LINE FEED]
+	beq $t2, 10, exit
+	
+	#Check if content = 0 or 1
+	blt $t2, '0', return_error
+	bgt $t2, '1', return_error
+	
+	j verify_loop
+
+return_error:
+	li $v0, 4
+	la $a0, error
+	syscall
+	j display_menu
+	
+binary_verify_finished:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
+	
 
 #Display give_input message
 #Take in input
@@ -97,8 +157,10 @@ from_hexadecimal:
 	li $v0, 4
 	syscall
 	
-	#Take Input
+	#Read Input
 	li $v0, 8
+	#Set Address of input buffer
+	la $a0, buffer
 	#Set MAX Characters to read
 	li $a1, 30
 	syscall
@@ -129,8 +191,10 @@ from_decimal:
 	li $v0, 4
 	syscall
 	
-	#Take Input
+	#Read Input
 	li $v0, 8
+	#Set Address of input buffer
+	la $a0, buffer
 	#Set MAX Characters to read
 	li $a1, 30
 	syscall
@@ -149,6 +213,14 @@ from_decimal:
 #If it DOES'T meet all criteria then jump to invalid_input method, display message and then jump back to display_menu 
 verify_decimal:
 
+
+display_results:
+
+	#Display Binary Message & Number
+	la $a0, binary
+	li $v0, 4
+	syscall
+	
 
 #Exit the application
 exit:
